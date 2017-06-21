3b673bcd7a4b7a834fcd0093bc669fd086e4dc68
Bradley Hilton <graywolf336@craftyn.com>
2017-06-21 16:17:25 -0500

    Add isApplicable to the preRoom

--- src/rooms/IPreRoomCreateHandler.ts
+++ src/rooms/IPreRoomCreateHandler.ts
@@ -1,5 +1,8 @@
+import { IRead } from '../accessors/index';
 import { IRoom } from './IRoom';
 
 export interface IPreRoomCreateHandler {
     preRoomCreate(room: IRoom): IRoom;
+
+    isApplicable(message: IRoom, read: IRead): boolean;
 }
