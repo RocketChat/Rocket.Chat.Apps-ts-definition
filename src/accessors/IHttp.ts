580990c5883f703f08237d1337248017d1cbcc34
Oliver Jägle <github@beimir.net>
2017-06-22 19:11:31 +0200

    Merge branch 'master' of https://github.com/graywolf336/temporary-rocketlets-ts-definition
    - changed iterator result
    - supplied more semantic methods to postMessageSent
    
    # Conflicts:
    #	src/accessors/IIterator.ts
    #	src/accessors/IRoomRead.ts
    #	src/accessors/index.ts

--- src/accessors/IHttp.ts
+++ src/accessors/IHttp.ts
@@ -1,3 +1,10 @@
 /**
- * Created by oliverjaegle on 22.06.17.
+ *
+ * there should actually already exist such an interface in some library.
+ * I would actually wrap it in order to have an implementation adding more comfort, such as adding a response header
  */
+export interface IHttp {
+    get(): boolean;
+
+    put(): boolean;
+}
--- src/messages/IPostMessageSentHandler.ts
+++ src/messages/IPostMessageSentHandler.ts
@@ -1,6 +1,5 @@
+import { IHttp } from '../accessors/IHttp';
 import { IRead } from '../accessors/index';
-import { IRoom } from '../rooms/index';
-import { IUser } from '../users/index';
 import { IMessage } from './IMessage';
 
 export interface IPostMessageSentHandler {
@@ -18,9 +17,7 @@ export interface IPostMessageSentHandler {
     /**
      * Method called *after* the message is sent to the other clients.
      *
-     * @param user The user who sent the message
-     * @param room The room where the message was sent to
      * @param message The message which was sent
      */
-    postMessageSent(user: IUser, room: IRoom, message: IMessage): void;
+    communicatePostMessageSent(message: IMessage, read: IRead, http: IHttp): void;
 }
