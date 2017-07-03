3044408e89cddd2aa655d0cd8258be101ae1c283
Bradley Hilton <graywolf336@craftyn.com>
2017-07-03 12:32:38 -0500

    Bump the version

---
+++ src/metadata/index.ts
@@ -0,0 +1,5 @@
+import { IRocketletAuthorInfo } from './IRocketletAuthorInfo';
+import { IRocketletInfo } from './IRocketletInfo';
+import { RocketChatAssociationModel } from './RocketChatAssociationModel';
+
+export { IRocketletAuthorInfo, IRocketletInfo, RocketChatAssociationModel };
