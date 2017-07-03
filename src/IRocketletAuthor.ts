211fe7a295b8fa37fd65cd8417e82321bf8246e5
Bradley Hilton <graywolf336@craftyn.com>
2017-07-03 01:51:14 -0500

    Add the author information to the json schema

--- src/IRocketletAuthor.ts
+++
@@ -1,8 +0,0 @@
-export interface IRocketletAuthor {
-    /** The name of who is the author/maintainer of this Rocketlet. */
-    name: string;
-    /** Where can the user get support? Website, email, telephone, etc. */
-    support: string;
-    /** Website link to the author's page, maybe a GitHub repository? */
-    homepage?: string;
-}
