7e38222caa58810cd886c71df0d4d830ad68e250
Bradley Hilton <graywolf336@craftyn.com>
2017-07-05 23:57:33 -0500

    Update the logging interfaces

--- src/accessors/ILogEntry.ts
+++ src/accessors/ILogEntry.ts
@@ -4,7 +4,12 @@ import { LogMessageSeverity } from './LogMessageSeverity';
  * Message which will be passed to a UI (either in a log or in the application's UI)
  */
 export interface ILogEntry {
-    severity: LogMessageSeverity;
+    /** The function name who did this logging, this is automatically added (can be null). */
+    caller?: string;
+    /** The severity rate, this is automatically added. */
+    severity?: LogMessageSeverity;
+    /** When this entry was made. */
     timestamp: Date;
+    /** The items which were logged. */
     args: Array<any>;
 }
--- src/accessors/ILogger.ts
+++ src/accessors/ILogger.ts
@@ -1,9 +1,18 @@
+import { ILogEntry } from './ILogEntry';
+
 /**
  * This logger provides a way to log various levels to the entire system.
  * When used, the items passed in will be logged to the database. This will
  * allow people to easily see what happened (users) or debug what went wrong.
  */
 export interface ILogger {
+    debugEntries: Array<ILogEntry>;
+    infoEntries: Array<ILogEntry>;
+    logEntries: Array<ILogEntry>;
+    warnEntries: Array<ILogEntry>;
+    errorEntries: Array<ILogEntry>;
+    successEntries: Array<ILogEntry>;
+
     debug(...items: Array<any>): void;
     info(...items: Array<any>): void;
     log(...items: Array<any>): void;
