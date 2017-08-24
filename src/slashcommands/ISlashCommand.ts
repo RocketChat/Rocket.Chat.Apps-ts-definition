935215d9d6a44ac21b511f7c6d80ac1e747c296e
Bradley Hilton <graywolf336@craftyn.com>
2017-08-24 13:30:28 -0500

    Commands can require permissions

--- src/slashcommands/ISlashCommand.ts
+++ src/slashcommands/ISlashCommand.ts
@@ -5,8 +5,14 @@ import { SlashCommandContext } from './SlashCommandContext';
  * Represents a slash command that is being provided.
  */
 export interface ISlashCommand {
+    /** The value which determines what the user types. */
     command: string;
+    /** Example of the parameters or an i18n string. */
     paramsExample: string;
+    /** i18n string for the description of the command. */
     i18nDescription: string;
+    /** Optional. Permission value required for the user to have to see/use it. */
+    permission?: string;
+    /** The function which gets called when a user enters the command. */
     executor(context: SlashCommandContext, builder: IBuilder, read: IRead, http: IHttp): void;
 }
