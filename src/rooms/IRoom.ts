9f6940b764a76819d7dfb628840f0aaa97120040
Bradley Hilton <graywolf336@craftyn.com>
2017-08-30 11:04:39 -0500

    Add some properties to the room interface

--- src/rooms/IRoom.ts
+++ src/rooms/IRoom.ts
@@ -3,13 +3,17 @@ import { RoomType } from './RoomType';
 
 export interface IRoom {
     id: string;
-    name: string;
+    displayName: string;
+    slugifiedName?: string;
     type: RoomType;
     creator: IUser;
     usernames: Array<string>;
     isDefault?: boolean;
+    isReadOnly?: boolean;
+    displaySystemMessages?: boolean;
     messageCount?: number;
     createdAt?: Date;
     updatedAt?: Date;
     lastModifiedAt?: Date;
+    customFields?: { [key: string]: object };
 }
