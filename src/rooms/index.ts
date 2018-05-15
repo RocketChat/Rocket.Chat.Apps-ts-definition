30f85c035190ff81f6a4b9f7d30df3d07ce61ed8
Bradley Hilton <graywolf336@craftyn.com>
2018-05-15 16:25:56 -0500

    Add the room deleted handlers

--- src/rooms/index.ts
+++ src/rooms/index.ts
@@ -1,7 +1,9 @@
 import { IPostRoomCreate } from './IPostRoomCreate';
+import { IPostRoomDeleted } from './IPostRoomDeleted';
 import { IPreRoomCreateExtend } from './IPreRoomCreateExtend';
 import { IPreRoomCreateModify } from './IPreRoomCreateModify';
 import { IPreRoomCreatePrevent } from './IPreRoomCreatePrevent';
+import { IPreRoomDeletePrevent } from './IPreRoomDeletePrevent';
 import { IRoom } from './IRoom';
 import { RoomType } from './RoomType';
 
@@ -9,7 +11,9 @@ export {
     IRoom,
     RoomType,
     IPostRoomCreate,
+    IPostRoomDeleted,
     IPreRoomCreateExtend,
     IPreRoomCreateModify,
     IPreRoomCreatePrevent,
+    IPreRoomDeletePrevent,
 };
