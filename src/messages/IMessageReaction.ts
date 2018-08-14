65e6f34e96e553059aa6a80b9073911203ae2386
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2018-08-14 16:34:39 -0500

    Fix fat finger error

--- src/messages/IMessageReaction.ts
+++ src/messages/IMessageReaction.ts
@@ -2,6 +2,6 @@
  * Interface which represents a reaction which can be added to a message.
  */
 export interface IMessageReaction {
-    /** Usernames of the users that reacted *
+    /** Usernames of the users that reacted */
     usernames?: Array<string>;
 }
